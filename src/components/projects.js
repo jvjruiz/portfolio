d68d439a95f1392279465d0affd917d66a0ba125
Jo Vincent Ruiz <jovincent.ruiz@gmail.com>
2016-12-29 09:00:47 -0800

    updated css a little more

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,17 +11,17 @@ export default class Projects extends Component {
 					<h1 id="projectsHeader">Projects</h1>
 					<ul id="projectList">
 						<li className="projectLeft project">
-							<h3 className="projectName">Game of Towers</h3>
+							<a href="https://gameoftowers.herokuapp.com/" className="projectName">Game of Towers</a>
 							<img src="" alt="insertprojectphothere"/>
 							<p className="projectDescription">Game of Towers is a simple tower defense game build with HTML Canvas and Javascript. A player builds towers to stop oncoming creeps from getting to the end of the maze. Players are able to make an account and login to save their high score.</p>
 						</li>
 						<li className="projectRight project">
-							<h3 className="projectName">Meteor Instant Messaging</h3>
+							<a href="https://meteor-chat-react.herokuapp.com/" className="projectName">Meteor Instant Messaging</a>
 							<img src="" alt="insertprojectphothere"/>
 							<p className="projectDescription">This is an instant messaging app, built with React and Meteor. Users are able to make an account and chat with others in real time. All of the messages are saved on a database for later viewing.</p>
 						</li>
 						<li className="projectLeft project">
-							<h3 className="projectName">Spaced Repition Quiz App</h3>
+							<a href="https://spaced-repetition-cloned-jvruiz.c9users.io/#/" className="projectName">Spaced Repition Quiz App</a>
 							<img src="" alt="insertprojectphothere"/>
 							<p className="projectDescription">This app is a quiz app that asks the user questions based on a spaced-reptition algorithm. The spaced-reptition algorithm asks the user questions by prioritizing questions that get answered incorrectly to maximize learning portential.</p>
 						</li>
