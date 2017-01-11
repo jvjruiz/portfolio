0584f20cc68d8d07ba4107e71844b0df6e3707cc
jvjruiz <jovincent.ruiz@gmail.com>
2017-01-10 19:59:58 -0800

    initial commit on branch

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,91 +1,41 @@
 import React, {Component} from 'react';
-import jsLogo from "../assets/logos/js.png";
-import htmlLogo from "../assets/logos/html5.png";
-import cssLogo from "../assets/logos/css3.png";
-import jqueryLogo from "../assets/logos/jquery.png";
-import meteorLogo from "../assets/logos/meteor.png";
-import reactLogo from "../assets/logos/react.png";
-import reduxLogo from "../assets/logos/redux.png";
-import mongoLogo from "../assets/logos/mongodb.png";
-import canvasLogo from "../assets/logos/canvas.png";
+
+import ImageGallery from 'react-image-gallery';
 
 export default class Projects extends Component {
+	handleImageLoad(event) {
+    	console.log('Image loaded ', event.target)
+  	}
+
 	render() {
+		const images = [
+	      {
+	        original: 'http://lorempixel.com/1000/600/nature/1/',
+	        thumbnail: 'http://lorempixel.com/250/150/nature/1/',
+	      },
+	      {
+	        original: 'http://lorempixel.com/1000/600/nature/2/',
+	        thumbnail: 'http://lorempixel.com/250/150/nature/2/'
+	      },
+	      {
+	        original: 'http://lorempixel.com/1000/600/nature/3/',
+	        thumbnail: 'http://lorempixel.com/250/150/nature/3/'
+	      }
+    	]
+
 		return(
 			<div id="projects">
 				<div id="technologies">
 					<div id="techonologiesContent">
-						<p id="technologiesHeader">Projects</p>
-						<div id="languages">
-							<ul id="languageList">
-								<li className="language">
-									<img src={htmlLogo} className="logo"/>
-								</li>
-								<li className="language">
-									<img src={cssLogo} className="logo"/>
-								</li>
-								<li className="language">
-									<img src={jsLogo} className="logo"/>
-								</li>
-							</ul>
-						</div>
-						<div id="api">
-							<ul id="apiList">
-								<li className="api">
-									<img src={meteorLogo} className="logo"/>
-								</li>
-								<li className="api">
-									<img src={reduxLogo} className="logo"/>
-								</li>
-								<li className="api">
-									<img src={reactLogo} className="logo"/>
-								</li>
-								<li className="api">
-									<img src={mongoLogo} className="logo"/>
-								</li>
-								<li className="api">
-									<img src={canvasLogo} className="logo"/>
-								</li>
-								<li className="api">
-									<img src={jqueryLogo} className="logo"/>
-								</li>
-							</ul>
-						</div>
+						<h1 id="technologiesHeader">Projects</h1>
 					</div>
 				</div>
-				<div className="content">
-					<ul id="projectList">
-						<li className="project">
-							<div className="projectContent">
-								<h1 className="projectName"><a href="https://gameoftowers.herokuapp.com/">Meteor Chat</a>
-								</h1>
-								<p className="projectDescription">Game of Towers is a simple tower defense game build with HTML Canvas and Javascript. A player builds towers to stop oncoming creeps from getting to the end of the maze. Players are able to make an account and login to save their high score.</p>
-							</div>
-							<div className="projectPhotoContainer">
-								<img src="" alt="insertprojectphothere" className="projectPhoto"/>
-							</div>
-						</li>
-						<li className="project">
-							<div className="projectContent">
-								<h1 className="projectName"><a href="https://gameoftowers.herokuapp.com/">Meteor Chat</a>
-								</h1>
-								<p className="projectDescription">Game of Towers is a simple tower defense game build with HTML Canvas and Javascript. A player builds towers to stop oncoming creeps from getting to the end of the maze. Players are able to make an account and login to save their high score.</p>
-							</div>
-							<div className="projectPhoto">
-								<img src="" alt="insertprojectphothere"/>
-							</div>
-						</li>
-						<li className="project">
-							<div className="projectContent">
-								<h1 className="projectName"><a href="https://gameoftowers.herokuapp.com/">Meteor Chat</a>
-								</h1>
-								<p className="projectDescription">Game of Towers is a simple tower defense game build with HTML Canvas and Javascript. A player builds towers to stop oncoming creeps from getting to the end of the maze. Players are able to make an account and login to save their high score.</p>
-							</div>
-							<div className="projectPhoto">
-								<img src="" alt="insertprojectphothere"/>
-							</div>
-						</li>
-					</ul>
+				<div>
+					<ImageGallery
+        			items={images}
+        			slideInterval={2000}
+        			onImageLoad={this.handleImageLoad}
+        			/>
 				</div>
 			</div>
 		)
