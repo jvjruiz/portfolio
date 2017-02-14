839f89b6dcd57793bb7f9f727968f5003b672abf
JR <jovincent.ruiz@gmail.com>
2017-02-14 01:21:33 -0800

    added responsive for landing page iphone6

--- src/components/intro.js
+++ src/components/intro.js
@@ -19,25 +19,29 @@ export default class Intro extends Component {
 		return (
 			<div id="intro" >
 				<div id="introContentContainer" className="content">
-					<h1 id="intro_message">
-						Hi, 
-						<br />
-						<span>I'm </span>
-						<span id="name">Jo Vincent</span>
-						<span>.</span>
-						<span id="blinking-cursor">|</span>
-					</h1>
-					<ul id="intro_icons" className="headerText">
-						<li className="intro_icon">
-							Leader.				
-						</li>
-						<li className="intro_icon">
-							Developer.			
-						</li>
-						<li className="intro_icon">
-							Communicator.				
-						</li>
-					</ul>
+					<div id="introMessageContainer">
+						<h1 id="intro_message">
+							Hi, 
+							<br />
+							<span>I'm </span>
+							<span id="name">Jo Vincent</span>
+							<span>.</span>
+							<span id="blinking-cursor">|</span>
+						</h1>
+					</div>
+					<div id="brandContainer">	
+						<ul id="intro_icons" className="headerText">
+							<li className="intro_icon">
+								Leader.				
+							</li>
+							<li className="intro_icon">
+								Developer.			
+							</li>
+							<li className="intro_icon">
+								Communicator.				
+							</li>
+						</ul>
+					</div>	
 				</div>
 			</div>
 		)
