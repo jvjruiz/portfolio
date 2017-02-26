cd5134a64c4a039280582b1d788e8a311d54c0ad
JR <jovincent.ruiz@gmail.com>
2017-02-26 11:25:21 -0800

    ??

--- src/components/projects.js
+++ src/components/projects.js
@@ -71,7 +71,7 @@ export default class Projects extends Component {
 								<p>HTML - CSS - Javascript - Meteor - React</p>
 							</div>
 							<div className="projectDescription contentText">
-								<p>Instant messaging app build with React and Meteor. This app was built in a week as a solo project to learn and get familiar with MeteorJS.
+								<p>Instant messaging app build with React and Meteor. This app was built in a week as a solo project. 
 								</p>
 							</div>
 							<div className="projectLinks contentText">
